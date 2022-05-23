08751553e6b83b821a25e955f699ca3632289b4d
diuwu.v3 <100076520+diuwuv3@users.noreply.github.com>
2022-05-23 11:22:44 +0200

    Separated variables to config.py

---
+++ config.py
@@ -0,0 +1,49 @@
+webhook_url = "" # Your Webhook URL
+
+tracker = { # Some examples
+    "Generation Free": {
+        "url": "https://generation-free.org/register/null", # Tracker REGISTRATION link, where there is a message if the tracker is closed
+        "msg": "Open Registration is Closed! You Must Be Invited To Register! You Have Been Redirected To Login Page!", # Closed message
+        "flag": ":flag_fr:" # Misc: country/language of the tracker
+    },
+    "FL-Team": {
+        "url": "https://flopload.cc/sbg_login_new.php",
+        "msg": "Désolé, mais les inscriptions sont fermées.",
+        "flag": ":flag_fr:"
+    },
+    "Pirata Digital": {
+        "url": "https://pirata.digital/register/null",
+        "msg": "Os registros estão fechados! Você deve ser convidado para se registrar! Você foi redirecionado para a página de Login!",
+        "flag": ":flag_br:"
+    },
+    "Br Society": {
+        "url": "https://brsociety.club/register/null",
+        "msg": "Os registros estão fechados! Você deve ser convidado para se registrar! Você foi redirecionado para a página de Login!",
+        "flag": ":flag_br:"
+    },
+    "Brasil Tracker": {
+        "url": "https://brasiltracker.org/register.php",
+        "msg": "Opa, somente registros com convite",
+        "flag": ":flag_br:"
+    },
+    "Amigos Share Club": {
+        "url": "https://cliente.amigos-share.club/account-signup.php",
+        "msg": "Codigo do convite",
+        "flag": ":flag_br:"
+    },
+    "Anime No Sekai": {
+        "url": "https://www.ansktracker.net/signup.php",
+        "msg": "No momento o tracker não aceita mais cadastros. Tente novamente outro dia.",
+        "flag": ":flag_br:"
+    },
+    "AnimeBytes": {
+        "url": "https://animebytes.tv/register/apply",
+        "msg": "Invite Application",
+        "flag": ":flag_gb:"
+    },
+    "GazelleGames": {
+        "url": "https://gazellegames.net/register.php",
+        "msg": "Registration is currently closed.",
+        "flag": ":flag_gb:"
+    }
+}
